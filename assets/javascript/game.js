bd5c8bb6b32aacbac4e5e15c444c871e3be74fca
unknown <jjernigan1065@gmail.com>
2018-03-07 18:28:27 -0500

    finishing up logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,47 +3,60 @@ var game = {
     losses: 0,
     numbersPicked: false,
     pictureMaker: ['assets/images/blue_crystal.jpg', 'assets/images/green_crystal.jpg', 'assets/images/pink_crystal.jpg','assets/images/purple_crystal.jpg' ],
-    crysNumTracker: 0,
-    winNumTracker: [],
+    crysNum: 0,
+    winNum: 0,
     gameStart: function () {
         if (this.numbersPicked === false) {
-            this.winNumTracker.push(Math.floor(Math.random()*(121-19)+19));
-            console.log("this is the winning random number: " + this.winNumTracker);
-            $(".randomNumber").text(this.winNumTracker);
-            $(".totalScore").text(this.crysNumTracker);
+            // function reset(){
+            this.winNum = Math.floor(Math.random()*(121-19)+19);
+
+            game.numbersPicked = true;
+            console.log("this is the winning random number: " + this.winNum);
+            $(".randomNumber").text(this.winNum);
+            $(".totalScore").text(this.crysNum);
+            $(".wins").text("Wins: "+this.wins);
+            $(".losses").text("Losses: "+this.losses);
             for(var i=0;i<this.pictureMaker.length;i++){
                 var randNum = Math.floor(Math.random()*(13-1)+1);
                 $('#box').append(
                     $('<img>')
-                        // .css("margin-right: 10px")
-                        // var randNum = Math.floor(Math.random()*(13-1)+1)
                         .attr('src', this.pictureMaker[i])
-                        // .attr('value', randNum)
                         .attr('data-value', randNum)
-                        // console.log($('<img>').val());
                         .addClass("crystal")
                 )
             }
         }
-
-
-                // console.log("this is the value of each img element: "+ $("<img>").davalue);
-
-            // console.log("this is the value of each img elemenet: "+ $("<img>").data());
             $('.crystal').on('click', function () {
                 var scoreValue = ($(this).attr('data-value'));
                 console.log(scoreValue);
-                game.crysNumTracker += parseInt(scoreValue);
+                game.crysNum += parseInt(scoreValue);
                 // game.score = game.score + scoreValue;
-                $('.totalScore').text(game.crysNumTracker);
+                $('.totalScore').text(game.crysNum);
+
+                console.log("this is the crystal number total: " + game.crysNum);
+                console.log("this is the winning number: "+game.winNum);
+                if(game.crysNum > game.winNum){
+                    game.losses++;
+                    $(".losses").text(game.losses);
+                    game.crysNum = 0;
+                    game.winNum = 0;
+                    game.numbersPicked = false;
+
+                }else if(game.crysNum === parseInt(game.winNum)){
+                    // console.log()
+                    game.wins++;
+                    $('.wins').text(game.wins);
+                    game.numbersPicked = false;
+
+                }
+
+            });
 
 
-            })
 
-        if(this.crysNumTracker === parseInt("this.")){}
 
     }
-}
+};
 
 console.log(game.pictureMaker);
 game.gameStart();

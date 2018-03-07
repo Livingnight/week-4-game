ecb7b6fcc6180c3d806aa9232554cd561084b651
unknown <jjernigan1065@gmail.com>
2018-03-07 11:21:28 -0500

    pictures working and sized. adding random value to each

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var game = {
     wins: 0,
     losses: 0,
     numbersPicked: false,
-    picturemaker: ['assets/images/blue_crystal.jpg', '../images/green_crystal.jpg', '../images/pink_crystal.jpg','../images/purple_crystal.jpg' ],
+    pictureMaker: ['assets/images/blue_crystal.jpg', 'assets/images/green_crystal.jpg', 'assets/images/pink_crystal.jpg','assets/images/purple_crystal.jpg' ],
     crysNumTracker: [],
     winNumTracker: [],
     gameStart: function () {
@@ -10,21 +10,27 @@ var game = {
             this.winNumTracker.push(Math.floor(Math.random()*(121-19)+19));
             console.log("this is the winning random number: " + this.winNumTracker);
             $(".randomNumber").text(this.winNumTracker);
-            for(i=0;i<this.picturemaker.length;i++){
+            for(var i=0;i<this.pictureMaker.length;i++){
                 $('#box').append(
                     $('<img>')
-                        .attr('src', 'picturemaker[i]')
+                        // .css("margin-right: 10px")
+                        .attr('src', this.pictureMaker[i])
+                        .val("Math.floor(Math.random()*(121-19)+19)")
+                        // console.log($('<img>').val());
                         .addClass("crystal")
+
+
                 );
+                console.log($('<img>').val());
             }
             $('.crystal').on('click', function () {
                 // this.winNumTracker.push(this.randWinNum);
                 // if($('#crystal-1').)
-                console.log("this is the win num: " + this.randWinNum);
+                // console.log("this is the win num: " + this.randWinNum);
 
             })
         }
     }
 }
-console.log(game.picturemaker);
+console.log(game.pictureMaker);
 game.gameStart();
